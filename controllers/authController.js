1d71cf1106c4623e11c576a1eb0f6e65e5e6ca4f
27manan10 <149328702+27manan10@users.noreply.github.com>
2025-12-25 05:56:51 +0530

    Refactor authController with consistent imports

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const User = require('../models/user');
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
@@ -112,5 +112,6 @@ const updateProfile = async (req, res) => {
     res.status(500).json({ message: 'Server Error' });
   }
 };
+
 
 module.exports = { registerUser, loginUser, getMe, addAddress, updateProfile };
